ae4211f526dca72436ecaad92f9e8fb7b3cc5852
srjizhere <bhiapatil@gmail.com>
2023-07-19 22:49:55 +0530

    sadf

--- controller/video.controller.js
+++ controller/video.controller.js
@@ -5,15 +5,15 @@ const videoController = async (req, res) => {
   try {
     const page = req.query.page || 1;
 
-    const totalCount = await VideoModel.countDocuments();
-    const totalPage = Math.ceil(totalCount / 10);
+    // const totalCount = await VideoModel.countDocuments();
+    // const totalPage = Math.ceil(totalCount / 10);
 
     const video = await VideoModel.find()
       .sort({ publishedDate: -1 })
       .skip((page - 1) * 10)
       .limit(10);
       // console.log({ currentPage: page, totalPage: totalPage, video });
-    res.send({ currentPage: page, totalPage: totalPage, video });
+    res.send({  video });
   } catch (error) {
     res.send({"error" :error.message});
   }
